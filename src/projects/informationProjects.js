71984d2c64404321c0fbcb6c8969092a3cbd2ef0
cleitoncarloss <cleitoncarlos.programmer@gmail.com>
2026-01-19 23:47:30 -0300

    feat: novo projeto adicionado

--- src/projects/informationProjects.js
+++ src/projects/informationProjects.js
@@ -1,5 +1,6 @@
 import autenticator from "./autenticator.png";
 import ecommerce from "./ecommerce.png";
+import tecboard from "./tecboard.png";
 
 const informationProject = [
   {
@@ -19,6 +20,15 @@ const informationProject = [
     stacks: ["REACT", "TAILWIND", "SUPABASE"],
     url: "https://autenticator.netlify.app/auth/sign-in",
     details: "Projeto de fluxo de autenticação desenvolvido em React, utilizando Supabase como backend-as-a-service para autenticação e Tailwind CSS para estilização. A aplicação implementa telas de login, criação de conta, recuperação de senha e verificação de código, com foco em componentização e gerenciamento de estado"
+  },
+  {
+    id: 3,
+    image: tecboard,
+    title: "Tecboard",
+    description: "Cadastro de enventos de tech",
+    stacks: ["REACT"],
+    url: "https://tecboardd.netlify.app/",
+    details: "Projeto desenvolvido em React utilizando JSX, componentização e useState para gerenciamento de estado. A aplicação consiste em um formulário de cadastro de eventos de tecnologia, permitindo ao usuário informar nome do evento, imagem, data e categoria onde os dados são capturados via form action com FormData."
   }
 ] 
 
